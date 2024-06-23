97a171047c6903cf04e79c59701069feab2e4788
chando-park <126308035+chando-park@users.noreply.github.com>
2024-06-23 11:04:50 +0900

    Initial Commit

---
+++ kidsnote_pre_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kidsnote_pre_project
+//
+//  Created by Chando Park on 6/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
